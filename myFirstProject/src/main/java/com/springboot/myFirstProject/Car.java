222ce76912590cbe90d441b55b20303cf49b9468
Vishal Kushwaha <vishalkushwaha2294@gmail.com>
2026-03-29 16:22:59 +0530

    understanding basics done

---
+++ myFirstProject/src/main/java/com/springboot/myFirstProject/Car.java
@@ -0,0 +1,17 @@
+package com.springboot.myFirstProject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Car {
+
+    @Autowired
+    private Dog dog; // dependency injection animal class dog class pe depend hai
+
+    @GetMapping("/ok")
+    public String ok(){
+        return dog.fun();
+    }
+}
---
+++ myFirstProject/src/main/java/com/springboot/myFirstProject/Dog.java
@@ -0,0 +1,10 @@
+package com.springboot.myFirstProject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog {
+    public String fun() {
+        return "Something";
+    }
+}
