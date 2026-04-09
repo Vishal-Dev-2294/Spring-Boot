000aad0de63cf7f065042f051877c481a0d468ba
Vishal Kushwaha <vishalkushwaha2294@gmail.com>
2026-04-09 12:41:54 +0530

    Doing basic stuff

--- journalApp/src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -1,4 +1,43 @@
 package net.engineeringdigest.journalApp.controller;
 
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/Journal")
 public class JournalEntryController {
+
+    private Map<Long, JournalEntry> JournalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll(){ // Get pe ya chalega
+        return new ArrayList<>(JournalEntries.values());
+    }
+
+    @PostMapping()
+    public boolean createEntry (@RequestBody JournalEntry myEntry){ // Post pe ye chalega
+        JournalEntries.put(myEntry.getId(), myEntry);
+        return true;
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable long myId){
+        return JournalEntries.get(myId);
+    }
+
+    @DeleteMapping("id/{myId}")
+    public JournalEntry deleteJournalEntryById(@PathVariable long myId){
+        return JournalEntries.remove(myId);
+    }
+
+    @PutMapping("/id/{myId}")
+    public JournalEntry updateJournalEntryById(@PathVariable long myId , @RequestBody JournalEntry myEntry){
+            return JournalEntries.put(myId , myEntry);
+    }
+
 }
